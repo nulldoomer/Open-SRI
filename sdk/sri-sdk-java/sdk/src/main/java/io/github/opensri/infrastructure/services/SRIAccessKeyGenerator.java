47d6c868907ca06d40727ec7d64e2ea3931dd691
nulldoomer <pauloarielyepez@gmail.com>
2026-03-24 23:21:34 -0500

    feat(java-sdk): add accesskeygenerator implementation

--- sdk/sri-sdk-java/sdk/src/main/java/io/github/opensri/infrastructure/services/SRIAccessKeyGenerator.java
+++ sdk/sri-sdk-java/sdk/src/main/java/io/github/opensri/infrastructure/services/SRIAccessKeyGenerator.java
@@ -1,4 +1,89 @@
 package io.github.opensri.infrastructure.services;
 
-public class SRIAccessKeyGenerator {
+import io.github.opensri.application.ports.AccessKeyGenerator;
+import io.github.opensri.domain.entities.common.DocumentNumber;
+import io.github.opensri.domain.entities.common.TaxInfo;
+import io.github.opensri.domain.enums.Environment;
+import io.github.opensri.domain.valueobjects.IssueDate;
+
+import java.util.Random;
+
+public class SRIAccessKeyGenerator implements AccessKeyGenerator {
+    @Override
+    public String generate(IssueDate date, DocumentNumber documentNumber,
+                           TaxInfo taxInfo, Environment environment) {
+
+        // Separación de campos que conforman la clave de acceso del SRI
+
+        String issueDate = date.format();
+
+        // Tipo de comprobante | Tabla Nro. 3 SRI
+        String documentCode = documentNumber.documentCode();
+        String rucNumber = taxInfo.issuer().ruc().number();
+        String environmentType = String.valueOf(environment.getCode());
+        String serie = documentNumber.establishment() + documentNumber.emissionPoint();
+        String sequential = documentNumber.sequentialNumber();
+
+        /*
+         Código Numérico random, creado por el SDK (Algoritmo para crear un
+         número de 8 dígitos aleatorios, con baja probabilidad de similitud)
+         */
+        String codeNumber = generateNumberCode();
+        String emissionType = String.valueOf(taxInfo.emissionType());
+
+        String keyWithoutDigit = issueDate + documentCode + rucNumber +
+                environmentType + serie + sequential + codeNumber + emissionType;
+
+        int digito = modulo11(keyWithoutDigit);
+
+        String finalKey = keyWithoutDigit + digito;
+
+        return finalKey;
+    }
+
+    /**
+     *
+     * @return codeNumber
+     */
+    private String generateNumberCode(){
+
+        // Últimos 5 dígitos del timestamp del sistema
+        String timeRandom = String.valueOf(System.currentTimeMillis());
+        timeRandom = timeRandom.substring(timeRandom.length() - 5);
+
+        // Generación de un numero random de 3 dígitos
+        int normalRandom = new Random().nextInt(1000);
+        String normalRandomString = String.format("%03d", normalRandom);
+
+        // Combinación final del codeNumber
+        return timeRandom + normalRandomString;
+    }
+
+    private int modulo11(String keyWithoutDigit){
+        int sum = 0;
+        int factor = 7;
+
+        String[] digits = keyWithoutDigit.split("");
+
+        for( String digit : digits){
+            sum += Integer.parseInt(digit) * factor;
+
+            factor = factor -1;
+
+            if(factor == 1)
+                factor = 7;
+        }
+
+        int verifierDigit = sum % 11;
+
+        verifierDigit = 11- verifierDigit;
+
+        if(verifierDigit == 11)
+            verifierDigit = 0;
+
+        if(verifierDigit == 10)
+            verifierDigit = 1;
+
+        return verifierDigit;
+    }
 }
